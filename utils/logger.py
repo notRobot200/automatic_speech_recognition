f27582219c6b93410917b6a6368ef0909927fc94
Muhammad Adam Majid <117140539+notRobot200@users.noreply.github.com>
2025-02-20 02:21:29 +0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,9 @@
-import logging
+# import logging
 
-def setup_logger():
-    """Configure and return the application logger."""
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    return logger
+# def setup_logger():
+#     """Configure and return the application logger."""
+#     logging.basicConfig(level=logging.INFO)
+#     logger = logging.getLogger(__name__)
+#     return logger
 
-logger = setup_logger()
+# logger = setup_logger()
